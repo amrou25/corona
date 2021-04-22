dc211c2e0a0b98e4f7bde7fe4b23f8319b35a7d0
amrou25 <asahinai754@gmail.com>
2021-04-22 21:53:52 +0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask,render_template,request
+from youtube_search import YoutubeSearch
+from pytube import YouTube
+from flask_cors import CORS
+from corona_python import Country
+app = Flask(__name__,template_folder='template')
+CORS(app)
+
+@app.route('/',methods=["POST","GET"])
+def hello_world():
+   
+    country = Country("oman")
+    a=country.today_cases() 
+    b=country.today_deaths()
+    c=country.today_recovered()
+    d=country.active()
+    e=country.total_cases()
+    f=country.total_tests()
+    
+    return render_template('index.html',test=f,total=e,cases=a,death=b,rec=c,active=d)
+
+@app.route('/api',methods=["POST","GET"])
+def res():
+    h2=request.args.get('srch')
+    country = Country(h2)
+    a1=country.today_cases()
+    b1=country.today_deaths()
+    c1=country.today_recovered()
+    d1=country.active()
+    e1=country.total_cases()
+    f1=country.total_tests()
+   
+    return render_template('data.html',test=f1,total=e1,cases=a1,death=b1,rec=c1,active=d1)
+
+
+@app.route('/all',methods=["POST","GET"])
+def apall():
+    h2=request.args.get('srch')
+    country = Country(h2)
+    g=country.get_all()
+    return g
+    
+if __name__ == "__main__":    
+   app.run(debug=True)
